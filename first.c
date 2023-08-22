dc66877ad0dda66cc86440d0dd8e3e3457c9398a
evilyn <evilyndeveloper@gmail.com>
2023-08-22 19:09:28 -0300

    first commit

---
+++ first.c
@@ -0,0 +1,16 @@
+// explicação de operadores matemáticos
+//  de atribuição
+int main()
+{
+    int x1, y1;
+    x1 = 3;
+    y1 = 2;
+    // x1 = y1 - x1;
+    x1 = x1 + y1;
+
+    int x2, y2;
+    x2 = 3;
+    y2 = 2;
+
+    x2 *= y2;
+}
